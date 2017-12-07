9f81d6b9b580509596af2b6103e4fde6d1f0b4bb
Nikiforos Botis <nb1217@tui.doc.ic.ac.uk>
2017-12-07 01:09:36 +0000

    Improve code quality, remove unecessary code

--- given/main.cc
+++ given/main.cc
@@ -5,6 +5,11 @@
 
 #include "helper.h"
 
+const int NUM_OF_INPUT_ARGUMENTS = 5;
+const int TIMEOUT_WAIT_SEC = 20;
+const int NUM_OF_SEMAPHORES = 3;
+const int INITIAL_ARRAY_LENGTH = 500;
+
 void *producer (void *id);
 void *consumer (void *id);
 
@@ -29,35 +34,47 @@ struct consumer_thread_data {
   int empty;
 };
 
+/* It is undestood that this way of creation of the array and then
+ * the usage of  only a small part of it is inefficient. It could have
+ * been declared as a pointer and then created on the heap. However, it 
+ * is believed that this is not in the scope of this exercise */
+
 // The circular queue which will be accessed by both producers and consumers
-int shared_buffer[100];
+int shared_buffer[INITIAL_ARRAY_LENGTH];
 
 int main (int argc, char **argv)
 {
 
-  // 1(a)
+  // Handling invalid number of command line arguments
+  if(argc != NUM_OF_INPUT_ARGUMENTS) {
+    printf("Invalid number of input parameters; Yous should give exactly 5 parameters: \n"
+           "program's name, the queue's size, "
+           "the number of jobs per producer, the number of producers and the number of "
+           "consumers \n");
+    return -1;
+  }
+
   int queue_size = check_arg(argv[1]);
   int number_of_jobs_per_producer = check_arg(argv[2]);
   int number_of_producers = check_arg(argv[3]);
   int number_of_consumers = check_arg(argv[4]);
 
-  // Handling of invalid input
+  // Handling invalid input values in arguments
   if(queue_size < 0) {
-    printf("Invalid value in the first parameter; queue size should be a non-negative integer");
-    exit(-1);
+    printf("Invalid value in the first parameter; queue size should be a non-negative integer \n");
+    return -1;
   } else if(number_of_jobs_per_producer < 0) {
-    printf("Invalid value in the second parameter; number of jobs per producer should be a non-negative integer");
-    exit(-1);
+    printf("Invalid value in the second parameter; number of jobs per producer should be a non-negative integer \n");
+    return -1;
   } else if(number_of_producers < 0) {
-    printf("Invalid value in the third parameter; number of producers should be a non-negative integer");
-    exit(-1);
+    printf("Invalid value in the third parameter; number of producers should be a non-negative integer \n");
+    return -1;
   } else if(number_of_consumers < 0) {
-    printf("Invalid value in the fourth parameter; number of consumers should be a non-negative integer");
-    exit(-1);
+    printf("Invalid value in the fourth parameter; number of consumers should be a non-negative integer \n");
+    return -1;
   }
 
-  // 1(b)
-
+  // Pass time as a parameter in order for srand to produce (pseudo-)random values.
   srand(time(NULL));
 
   // Initialize the queue with 0, representing the empty values
@@ -68,20 +85,22 @@ int main (int argc, char **argv)
   // Initialize indexes in order to keep track of the next item in each case
   int producer_index = 0;
   int consumer_index = 0;
-
-  // 1(c)
   
-  // Num of each created semaphore
+  // Number of each created semaphore
   int mutex = 0;
   int full = 1;
   int empty = 2;
 
   // Creation of the 3 Semaphores that will be needed
   int sem_id;
-  sem_id = sem_create(SEM_KEY, 3);
-  
+  sem_id = sem_create(SEM_KEY, NUM_OF_SEMAPHORES);
+
+  if(sem_id == -1) {
+    printf("Error occured while creating the semaphore set");
+    return -1;
+  }
+
   // Initialization of the Semaphores
-  
   int init1 = sem_init(sem_id, mutex, 1);
   int init2 = sem_init(sem_id, full, 0);
   int init3 = sem_init(sem_id, empty, queue_size);
@@ -89,11 +108,10 @@ int main (int argc, char **argv)
   if((init1 == 0) && (init2 == 0) && (init3 == 0)) {
     printf("Semaphores successfully created \n");
   } else {
-    printf("Error occured during semaphore initialization \n");
+    printf("Error occured during the semaphore's initialization \n");
     exit(-1);
   }
 
-  // 1(d)
   pthread_t producersid[number_of_producers];
 
   struct producer_thread_data producer_data[number_of_producers];
@@ -126,8 +144,6 @@ int main (int argc, char **argv)
  
     pthread_create(&consumersid[i], NULL, consumer, (void*) &consumer_data[i]);
   }
-
-  // 1(e)
 
   // Ensure that all threds are done
   for(int i = 0; i < number_of_producers; i++) {
@@ -176,24 +192,25 @@ void *producer(void *parameter)
 
   while(jobs_produced < num_of_jobs_per_producer) {
 
+    // Consumer producers job
     job_dur = produce_job();
 
-    errno = sem_time_wait(sem_id, empty, 20);
+    errno = sem_time_wait(sem_id, empty, TIMEOUT_WAIT_SEC);
 
+    // If the queue is full even after the waiting time of 20", quit.
     if(errno == -1) {
-      printf("Producer (%i): The time interval of 20 seconds was exceeded without any new job being consumed \n", thread_id); 
+      printf("Producer (%i): The time interval of 20 seconds was exceeded without"
+	     "any new job being consumed \n", thread_id + 1); 
       break;
     }
 
     sem_wait(sem_id, mutex);
+
     shared_buffer[*producer_index] = job_dur;
  
-    // cout << "Shared buffer current state: ";
-    // for(int i = 0; i < queue_size; i++)
-    //   cout << shared_buffer[i] << " ";
-    // cout << endl;
+    printf("Producer(%i): Job id %i duration %i \n", (thread_id + 1), *producer_index,
+	   shared_buffer[*producer_index]);
 
-    printf("Producer(%i): Job id %i duration %i \n", thread_id, *producer_index, shared_buffer[*producer_index]);
     (*producer_index)++;
 
     if(*producer_index == queue_size) {
@@ -201,17 +218,16 @@ void *producer(void *parameter)
     }
     
     jobs_produced++;
+
     sem_signal(sem_id, mutex);
     sem_signal(sem_id, full);
-
-    //cout << "Value of full in producer is " << semctl(sem_id, full, GETVAL) << endl;
 
     next_job_produced_in = rand() % 5 + 1;
     sleep(next_job_produced_in);
   }
 
   if((errno != -1) && (jobs_produced == num_of_jobs_per_producer)) {
-    printf("Producer(%i): No more jobs to generate. \n", thread_id);
+    printf("Producer(%i): No more jobs to generate. \n", thread_id + 1);
   }
 
   pthread_exit(0);
@@ -220,7 +236,6 @@ void *producer(void *parameter)
 
 void *consumer (void *parameter)
 {
-
   struct consumer_thread_data *received_data;
 
   received_data = (struct consumer_thread_data *) parameter;
@@ -239,10 +254,10 @@ void *consumer (void *parameter)
 
   while(true) {
 
-    errno = sem_time_wait(sem_id, full, 20);
+    errno = sem_time_wait(sem_id, full, TIMEOUT_WAIT_SEC);
 
     if(errno == -1) {
-      printf("Consumer(%i): No more jobs left \n", thread_id);
+      printf("Consumer(%i): No more jobs left \n", thread_id + 1);
       break;
     }
 
@@ -250,14 +265,14 @@ void *consumer (void *parameter)
 
     duration = shared_buffer[*consumer_index];
 
-    while(!duration) {
-      //*consumer_index++;
+    // If there is no job in this location (duration = 0), move to the next
+    while(!duration)
       duration = shared_buffer[++*consumer_index];
-    }
 
     // Job removed and 0 (signal of no existing jobs) was placed for its duration
     shared_buffer[*consumer_index] = 0;
-    printf("Consumer(%i): Job id %i executing sleep duration %i \n", thread_id, *consumer_index, duration);
+    printf("Consumer(%i): Job id %i executing sleep duration %i \n", (thread_id + 1),
+	   *consumer_index, duration);
     id_just_finished = *consumer_index;
 
     (*consumer_index)++;
@@ -270,13 +285,8 @@ void *consumer (void *parameter)
 
     // This has the meaning of "consume the removed item"
     sleep(duration);
-    printf("Consumer(%i): Job id %i completed \n", thread_id, id_just_finished);
+    printf("Consumer(%i): Job id %i completed \n", (thread_id + 1), id_just_finished);
   }
-
-  // // The 20" interval has passed withouth having any new job to consumer
-  // if(errno == -1) {
-  //   printf("Consumer(%i): The time interval of 20 seconds was exceeded without any new job being produced \n", thread_id);
-  // }
 
   pthread_exit (0);
 }
